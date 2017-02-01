91e5f1755f73df155cf26cfa0a760a29a9536185
Cecilia Ulrich <cecilia.ulrich@logicline.de>
2017-02-01 12:19:40 +0100

    Node.js cluster module

--- server.js
+++ server.js
@@ -1,63 +1,87 @@
-var restify = require('restify');
-
-// State
-var next_user_id = 0;
-var users = {};
-
-var port = process.env.PORT || 8090;
-
-// Server
-var server = restify.createServer({
-  name: 'nodeRestify',
-  version: '1.0.0'
-});
-server.use(restify.acceptParser(server.acceptable));
-server.use(restify.queryParser());
-server.use(restify.bodyParser());
-
-server.get("/", function (req, res, next) {
-  res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
-  res.end(JSON.stringify(users));
-  return next();
-});
-
-server.get('/user/:id', function (req, res, next) {
-  res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
-  res.end(JSON.stringify(users[parseInt(req.params.id)]));
-  return next();
-});
-
-server.post('/user', function (req, res, next) {
-  var user = req.params;
-  user.id = next_user_id++;
-  users[user.id] = user;
-  res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
-  res.end(JSON.stringify(user));
-  return next();
-});
-
-server.put('/user/:id', function (req, res, next) {
-  var user = users[parseInt(req.params.id)];
-  var changes = req.params;
-  delete changes.id;
-  for(var x in changes) {
-    user[x] = changes[x];
+var cluster = require('cluster');
+if (cluster.isMaster)
+{
+  console.log('Server is active. Forking workers now.');
+  var cpuCount = require('os').cpus().length;
+  for (var i=0; i<cpuCount; i++)
+  {
+    cluster.fork();
   }
-  res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
-  res.end(JSON.stringify(user));
-  return next();
-});
+  cluster.on('exit', function(worker)
+  {
+    console.error('Worker %s has died! Creating a new one.', worker.id);
+    cluster.fork();
+  });
+}
+else {
+  var restify = require('restify');
 
-server.del('/user/:id', function (req, res, next) {
-  delete users[parseInt(req.params.id)];
-  res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
-  res.end(JSON.stringify(true));
-  return next();
-});
+  var port = process.env.PORT || 8090;
 
-server.listen(port, function () {
-  console.log('%s listening at %s', server.name, server.url);
-});
+  // State
+  var next_user_id = 0;
+  var users = {};
+
+  // Server
+  var server = restify.createServer({
+    name: 'nodeRestify',
+    version: '1.0.0'
+  });
+  server.use(restify.acceptParser(server.acceptable));
+  server.use(restify.queryParser());
+  server.use(restify.bodyParser());
+
+  server.get("/", function (req, res, next) {
+    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+    res.end(JSON.stringify(users));
+    console.log('Worker: '+ cluster.worker.id);
+    cluster.worker.kill();
+    return next();
+  });
+
+  server.get('/user/:id', function (req, res, next) {
+    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+    res.end(JSON.stringify(users[parseInt(req.params.id)]));
+    console.log('Worker: '+ cluster.worker.id);
+    return next();
+  });
+
+  server.post('/user', function (req, res, next) {
+    var user = req.params;
+    user.id = next_user_id++;
+    users[user.id] = user;
+    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+    res.end(JSON.stringify(user));
+    console.log('Worker: '+ cluster.worker.id);
+    return next();
+  });
+
+  server.put('/user/:id', function (req, res, next) {
+    var user = users[parseInt(req.params.id)];
+    var changes = req.params;
+    delete changes.id;
+    for(var x in changes) {
+      user[x] = changes[x];
+    }
+    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+    res.end(JSON.stringify(user));
+    console.log('Worker: '+ cluster.worker.id);
+    return next();
+  });
+
+  server.del('/user/:id', function (req, res, next) {
+    delete users[parseInt(req.params.id)];
+    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+    res.end(JSON.stringify(true));
+    console.log('Worker: '+ cluster.worker.id);
+    return next();
+  });
+
+  server.listen(port, function () {
+    console.log('Worker %s spawned for port %s.', cluster.worker.id, port);
+  });
+
+}
 
 /*
 // Client
